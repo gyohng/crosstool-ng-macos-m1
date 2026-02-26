df8d9d628307fccbcf5d0145955e8ada799fc95d
George Yohng <georgegitlab@oss3d.com>
2026-02-26 03:01:07 +0100

    toolchain for cmake

---
+++ darwin-build-configs/aarch64-poky-linux/toolchain.cmake
@@ -0,0 +1,21 @@
+cmake_minimum_required(VERSION 3.13)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CROSS_COMPILE_TOOLCHAIN_PREFIX "aarch64-poky-linux")
+set(CMAKE_CROSSCOMPILING_EMULATOR qemu-aarch64)
+
+set(CMAKE_C_COMPILER   /opt/cross/${CROSS_COMPILE_TOOLCHAIN_PREFIX}/bin/${CROSS_COMPILE_TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER /opt/cross/${CROSS_COMPILE_TOOLCHAIN_PREFIX}/bin/${CROSS_COMPILE_TOOLCHAIN_PREFIX}-g++)
+
+# where is the target environment 
+set(CMAKE_FIND_ROOT_PATH  /opt/cross/${CROSS_COMPILE_TOOLCHAIN_PREFIX})
+
+# search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+find_program(CMAKE_C_COMPILER_LAUNCHER ccache)
+find_program(CMAKE_CXX_COMPILER_LAUNCHER ccache)
